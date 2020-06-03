72cf5a50ff171ce5b3ac1882becb7699af2215e3
Carlos Catly <ccatly@gmail.com>
2020-06-02 17:23:56 -0700

    everything seems to be working. About to push up to heroku

--- routes/codes.js
+++ routes/codes.js
@@ -3,27 +3,24 @@ const Code = require('../models/codes');
 const router = express.Router();
 
 router.get('/codes', (req, res) => {
+    Code.find().then(codes => res.json(codes)).catch(err => {
+        console.log(err)
+        res.status(400).json(err);
+    })
+})
 
+router.post('/get', (req, res) => {
 
+    const newCode = new Code({
+        status: false
+    })
 
-    try {
-        // const codes = Code.find({});
-
-        // return res.json({
-        //     codes
-        // });
-
-        return Codes.find().then(codes => {
-            res.json({codes})
-        })
-
-
-    } catch (error) {
-        return res.status(400).json(error);
-    }
+    newCode.save().then(code => res.json({success: true, code})).catch(err => {
+        console.log(err)
+        res.status(400).json(err);
+    })
 })
 
 
-
-
 module.exports = router;
+
